3c74f8be388a8692c8498120b9928fb6e768e13b
X1X71 <167025686+X1X71@users.noreply.github.com>
2024-05-05 18:58:58 -0400

    Update generate_qr_code.py

--- generate_qr_code.py
+++ generate_qr_code.py
@@ -4,8 +4,10 @@ website_link = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
 
 # integer from 1 to 40 that controls the size of the QR code
 version = int(input("What size would you like your QR code to be? Enter a number from 1-40: "))
+
 # integer that controls the size of each box in the QR code
 box_size = int(input("How many pixels do you want each 'box' in the QR code? "))
+
 # integer that controls the thickness of the border around the QR code
 border = int(input("How many boxes thick do you want the border to be? "))
 
@@ -13,8 +15,12 @@ qr = qrcode.QRCode(version=version,
                         error_correction=qrcode.constants.ERROR_CORRECT_M, 
                         box_size=box_size, 
                         border=border)
+
+# replace 'website_link' with site you want qr code to redirect to
 qr.add_data(website_link)
 qr.make(fit=True)
 
 img = qr.make_image(fill_color = 'black', back_color = 'white')
+
+# saves qr code to computer as .png
 img.save('youtube_qr.png')
